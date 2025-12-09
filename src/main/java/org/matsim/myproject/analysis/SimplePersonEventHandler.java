597be2b5fe110286ce729100d86caed1041e5479
bernd886 <michael.schulze@campus.tu-berlin.de>
2025-12-09 20:27:27 +0100

    copy SimpleAnalysis package from serengeti, adding general comments

---
+++ src/main/java/org/matsim/myproject/analysis/SimplePersonEventHandler.java
@@ -0,0 +1,44 @@
+package org.matsim.myproject.analysis;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.PersonArrivalEvent;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.population.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// handling departureEvent by putting time and personID in Map
+// handling arrivalEvent by reading arrivalTime for personID
+// subtracting corresponding departureTime
+// print out
+
+public class SimplePersonEventHandler implements PersonArrivalEventHandler, PersonDepartureEventHandler {
+
+    // a simple var would be overridden by other events
+    private final Map<Id<Person>, Double> personToDepartureTime = new HashMap<>();
+
+    @Override
+    public void handleEvent( PersonDepartureEvent personDepartureEvent ) {
+
+        var departureTime = personDepartureEvent.getTime();
+        var personId = personDepartureEvent.getPersonId();
+
+        personToDepartureTime.put( personId, departureTime );
+
+        // System.out.println("Departure: " + personDepartureEvent.getTime() + ": " + personDepartureEvent.getPersonId());
+    }
+
+    @Override
+    public void handleEvent( PersonArrivalEvent personArrivalEvent ) {
+        var departureTime = personToDepartureTime.get( personArrivalEvent.getPersonId() );
+        var arrivalTime = personArrivalEvent.getTime();
+        // after a agent arrives, one can calculate the travel time
+        var travelTime = arrivalTime - departureTime;
+
+        System.out.println("Person " + personArrivalEvent.getPersonId() + " travelled " + travelTime + " seconds.");
+        //System.out.println("Arrival: " + personArrivalEvent.getTime() + ": " + personArrivalEvent.getPersonId());
+    }
+}
