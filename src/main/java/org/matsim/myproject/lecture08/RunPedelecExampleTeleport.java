4fdf144e275c860e8e377d641638376551d7e23a
bernd886 <michael.schulze@campus.tu-berlin.de>
2026-03-11 15:50:29 +0100

    Lecture 7 - adding Multimodality - starting with teleporting

---
+++ src/main/java/org/matsim/myproject/lecture08/RunPedelecExampleTeleport.java
@@ -0,0 +1,97 @@
+package org.matsim.myproject.lecture08;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.ReplanningConfigGroup;
+import org.matsim.core.config.groups.RoutingConfigGroup;
+import org.matsim.core.config.groups.ScoringConfigGroup;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.scoring.functions.ModeUtilityParameters;
+import org.matsim.core.utils.collections.CollectionUtils;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.examples.ExamplesUtils;
+
+public class RunPedelecExampleTeleport {
+
+    public static void main( String[] args ) {
+
+        var url = IOUtils.extendUrl( ExamplesUtils.getTestScenarioURL( "equil" ), "config.xml" );
+        Config config = ConfigUtils.loadConfig( url );
+        config.controller().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
+
+        config.controller().setLastIteration( 1 );
+
+        // plans innovation (or "strategy"):
+        {
+            // putting in a mode choice module
+            ReplanningConfigGroup.StrategySettings params = new ReplanningConfigGroup.StrategySettings();
+            params.setStrategyName( DefaultPlanStrategiesModule.DefaultStrategy.ChangeSingleTripMode );
+            params.setWeight( 1. ); // is high to see effect of mode change
+            config.replanning().addStrategySettings( params );
+        }
+        // configuring mode choice module
+        final String[] modes = { "car", "pedelec" };
+        config.changeMode().setModes( modes );
+
+        // routing:
+
+        // when implementing new modes, we are using teleport
+        // when teleporting, pre-existing are removed, clear for no errors
+        config.routing().clearTeleportedModeParams();
+        {
+            RoutingConfigGroup.TeleportedModeParams params = new RoutingConfigGroup.TeleportedModeParams( "pedelec" );
+            params.setTeleportedModeSpeed( 15. / 3.6 );
+            params.setBeelineDistanceFactor( 1.3 );
+            config.routing().addTeleportedModeParams( params );
+        }
+        {
+            RoutingConfigGroup.TeleportedModeParams params = new RoutingConfigGroup.TeleportedModeParams( "walk" );
+            params.setTeleportedModeSpeed( 5. / 3.6 );
+            params.setBeelineDistanceFactor( 1.3 );
+            config.routing().addTeleportedModeParams( params );
+        }
+
+        // scoring:
+        {
+            ScoringConfigGroup.ModeParams params = new ScoringConfigGroup.ModeParams( "pedelec" );
+            params.setMarginalUtilityOfTraveling( 0. );
+            config.scoring().addModeParams( params );
+        }
+        {
+            ScoringConfigGroup.ModeParams params = new ScoringConfigGroup.ModeParams( "car" );
+            params.setMarginalUtilityOfTraveling( 0. );
+            config.scoring().addModeParams( params );
+        }
+
+        Scenario scenario = ScenarioUtils.loadScenario( config );
+        Controler controler = new Controler( scenario );
+        controler.run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
