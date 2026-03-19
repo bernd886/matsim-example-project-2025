3a8848dfacce2a1127979373a3437f60013452bb
bernd886 <michael.schulze@campus.tu-berlin.de>
2026-03-19 18:12:38 +0100

    RunDrtExample comments

--- src/main/java/org/matsim/myproject/lecture09/RunDrtExample.java
+++ src/main/java/org/matsim/myproject/lecture09/RunDrtExample.java
@@ -4,6 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contrib.common.zones.systems.grid.square.SquareGridZoneSystemParams;
 import org.matsim.contrib.drt.optimizer.insertion.extensive.ExtensiveInsertionSearchParams;
 import org.matsim.contrib.drt.routing.DrtRoute;
@@ -55,21 +59,41 @@ public class RunDrtExample {
             config.controller().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
         }
 
-        config.controller().setLastIteration( 1 );
+        config.controller().setLastIteration( 5 );
 
         config.qsim().setSimStarttimeInterpretation( QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime );
-        config.qsim().setInsertingWaitingVehiclesBeforeDrivingVehicles(true);
-        config.qsim().setSnapshotStyle(SnapshotStyle.queue);
+        config.qsim().setInsertingWaitingVehiclesBeforeDrivingVehicles( true ); // necessary
+        // KinWaves "best setting"
+        config.qsim().setTrafficDynamics( QSimConfigGroup.TrafficDynamics.kinematicWaves );
+        config.qsim().setSnapshotStyle( SnapshotStyle.kinematicWaves );
 
         @SuppressWarnings("unused")
+        // Base config
         DvrpConfigGroup dvrpConfig = ConfigUtils.addOrGetModule( config, DvrpConfigGroup.class );
-        dvrpConfig.getTravelTimeMatrixParams().addParameterSet(new SquareGridZoneSystemParams());
+        dvrpConfig.getTravelTimeMatrixParams().addParameterSet( new SquareGridZoneSystemParams() );
         // (config group needs to be "materialized")
 
+        // Definition of different taxi companies
         MultiModeDrtConfigGroup multiModeDrtCfg = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
+        // Clear all xml data, start over and construct again
+        // multiModeDrtCfg.getModalElements().clear();
+
+        // MODIFY: modes
+        {
+            // Has multiple elements (config for each mode)
+            // "stopDuration" ~ boarding time
+            // condition for rejection
+            // "maxWaitTime" ~ until taxi arrives for boarding
+            // "maxTravelTimeAlpha" ~ (%) how much longer trip is allowed to take, compared to a direct trip
+            // "maxTravelTimeBeta" ~ (s) how much longer trip is allowed to take, compared to a direct trip
+//            for ( DrtConfigGroup modalConfig : multiModeDrtCfg.getModalElements() ) {
+//                modalConfig.
+//            }
+        }
+
         {
             DrtConfigGroup drtConfig = new DrtConfigGroup();
-            drtConfig.setMode(DRT_A);
+            drtConfig.setMode( DRT_A );
             drtConfig.setStopDuration(60.);
             drtConfig.addOrGetDrtOptimizationConstraintsParams().addOrGetDefaultDrtOptimizationConstraintsSet().addParam( "maxWaitTime", String.valueOf( 900 ) );
             drtConfig.addOrGetDrtOptimizationConstraintsParams().addOrGetDefaultDrtOptimizationConstraintsSet().addParam( "maxTravelTimeAlpha", String.valueOf( 1.3 ) );
@@ -128,13 +152,33 @@ public class RunDrtExample {
             config.replanning().addStrategySettings( new StrategySettings().setStrategyName( DefaultSelector.ChangeExpBeta ).setWeight( 1. ) );
         }
 
-        // ===
-        Scenario scenario = ScenarioUtils.createScenario( config ) ;
+        // MODIFY scoring to obtain a mode choice reaction
+        {
+//            ModeParams params = config.scoring().getModes().get( DRT_A );
+//            params.setConstant( -100 );
+        }
+
+        // === Scenario
+        Scenario scenario = ScenarioUtils.createScenario( config );
+        // Since injector lives at controler level, one has to announces, that able to handle drt routing
         scenario.getPopulation().getFactory().getRouteFactories().setRouteFactory( DrtRoute.class, new DrtRouteFactory() );
         ScenarioUtils.loadScenario( scenario );
         // yyyy in long run, try to get rid of the route factory thing
 
-        // ===
+        // MODIFY scenario
+        {
+//            for ( Person person : scenario.getPopulation().getPersons().values() ) {
+//                // let everybody want to depart at same time
+//                Activity firstActivity = (Activity) person.getSelectedPlan().getPlanElements().get( 0 );
+//                firstActivity.setEndTime( 0 );
+//                // let everyone switch to the same mode. so there is only one taxi.
+//                Leg firstLeg = (Leg) person.getSelectedPlan().getPlanElements().get( 1 );
+//                firstLeg.setMode( DRT_A );
+//            }
+        }
+
+
+        // === Controler
         Controler controler = new Controler( scenario ) ;
 
         controler.addOverridingModule( new DvrpModule() ) ;
@@ -147,7 +191,7 @@ public class RunDrtExample {
             OTFVisConfigGroup otfVisConfigGroup = ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.class);
             otfVisConfigGroup.setLinkWidth(5);
             otfVisConfigGroup.setDrawNonMovingItems(true);
-            controler.addOverridingModule(new OTFVisLiveModule());
+            // controler.addOverridingModule(new OTFVisLiveModule());
         }
 
         controler.run() ;
